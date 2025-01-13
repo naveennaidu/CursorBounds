9ee2b6221d26fe8378caa1ffaa96718ba203bd6f
naveennaidu <naveen@zeitalabs.com>
2025-01-13 11:19:39 +0530

    update package.swift

--- Package.swift
+++ Package.swift
@@ -8,15 +8,16 @@ let package = Package(
         .macOS(.v12)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "CursorBounds",
             targets: ["CursorBounds"]),
     ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CursorBounds")
+            name: "CursorBounds",
+            dependencies: [],
+            path: "Sources/CursorBounds"
+        )
     ]
 )
